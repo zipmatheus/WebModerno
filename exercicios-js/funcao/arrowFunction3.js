b61f6e5a19cedffbd88effdd535bd312214cd028
matheus barbosa <zipmatheus@hotmail.com>
2023-01-03 17:00:25 -0300

    arrow functions

---
+++ exercicios-js/funcao/arrowFunction3.js
@@ -0,0 +1,12 @@
+let comparaComThis = function (param) {
+    console.log(this === param)
+}
+
+comparaComThis(global);
+
+const obj = {}
+comparaComThis = comparaComThis.bind(obj);
+comparaComThis(global);
+comparaComThis(obj);
+
+let comparaComThisArrow = param => console.log(this === param);
